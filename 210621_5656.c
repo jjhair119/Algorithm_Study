aa48f68e99cb92a9e84d151bcbbf4ca90055b2ab
jjhair119 <jmkjmk6666@gmail.com>
2021-06-21 22:28:05 +0900

    20210621_5656

---
+++ 210621_5656.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    int i=1, a, b;
+    char e[3];
+
+    while(1){
+        scanf("%d %s %d", &a, e, &b);
+
+        if ((i>12000)||!strcmp(e, "E"))
+            break;
+
+        if(!strcmp(e,">")){
+            if(a>b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+        if(!strcmp(e,">=")){
+            if(a>=b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+        if(!strcmp(e,"<")){
+            if(a<b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+        if(!strcmp(e,"<=")){
+            if(a<=b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+        if(!strcmp(e,"==")){
+            if(a==b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+        if(!strcmp(e,"!=")){
+            if(a!=b) printf("Case %d: true\n", i++);
+            else printf("Case %d: false\n", i++);
+        }
+
+        
+    }
+
+    return 0;
+}
