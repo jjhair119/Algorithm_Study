16ed7112b79e10109746f494c2cb34fabe33837e
지민경 <jmkjmk6666@gmail.com>
2021-06-24 23:59:49 +0900

    Create 210624_4880.c

---
+++ 210624_4880.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	
+	while(1){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a == 0 && b == 0 && c == 0)
+      break;
+
+    if(2 * b == a + c)
+      printf("AP %d\n", 2 * c - b);
+    else
+      printf("GP %d\n", c*c/b);
+
+  }
+
+	return 0;
+}
