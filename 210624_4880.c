3d9a1a26ba4c8b08d05cce57be3bb3e6e9d818bd
지민경 <jmkjmk6666@gmail.com>
2021-06-25 00:21:48 +0900

    2021.06.24 4880

--- 210624_4880.c
+++ 210624_4880.c
@@ -4,18 +4,18 @@
 int main() {
 	
 	while(1){
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+		int a, b, c;
+    		scanf("%d %d %d", &a, &b, &c);
 
-    if(a == 0 && b == 0 && c == 0)
-      break;
+    		if(a == 0 && b == 0 && c == 0)
+     		break;
 
-    if(2 * b == a + c)
-      printf("AP %d\n", 2 * c - b);
-    else
-      printf("GP %d\n", c*c/b);
+		if(2 * b == a + c)
+			printf("AP %d\n", 2 * c - b);
+		else
+		        printf("GP %d\n", c*c/b);
 
-  }
+  	}
 
 	return 0;
 }
